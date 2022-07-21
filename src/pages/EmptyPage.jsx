5d8aaa6fb7f380ac103ea376f8202282054fdd38
QuebecNova <selllmenya@gmail.com>
2022-07-22 00:00:56 +0300

    added back buttons

--- src/pages/EmptyPage.jsx
+++ src/pages/EmptyPage.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import Button from '../components/UI/Button'
 
 export default function EmptyPage() {
+  
+  const navigation = useNavigate()
+
   return (
-    <h1>Тут ничего нет! Вы не ошиблись?</h1>
+    <>
+      <Button onClick={() => navigation('/posts')}> К постам </Button>
+      <h1>Тут ничего нет! Вы не ошиблись?</h1>
+    </>
   )
 }
--- src/pages/PostDetailed.jsx
+++ src/pages/PostDetailed.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux/es/exports'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import Comment from '../components/Posts/Comment'
+import Button from '../components/UI/Button'
 import Loader from '../components/UI/Loader'
 import { fetchPostById, fetchPostCommentsById } from '../store/slices/actionCreators'
 
@@ -10,6 +11,7 @@ export default function PostDetailed() {
     const dispatch = useDispatch()
     const posts = useSelector(state => state.posts)
     const params = useParams()
+    const navigate = useNavigate()
 
     useEffect(() => {
         dispatch(fetchPostById(params.id))
@@ -24,6 +26,7 @@ export default function PostDetailed() {
 
   return (
     <div>
+        <Button onClick={() => navigate('/posts')}>Назад</Button>
         {posts.isLoading ? <Loader/> 
         :
         <>
